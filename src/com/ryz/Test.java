43fcadf37c4723433ea31050ea5b94d20ce63048
Ren <ren2295518977@qq.com>
2019-10-24 12:30:03 +0800

    在idel中创建TestGit项目!

--- src/com/ryz/Test.java
+++ src/com/ryz/Test.java
@@ -3,6 +3,6 @@ package com.ryz;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("在idel中创建项目111");
+        System.out.println("在idel中创建项目");
     }
 }
